ba19f538a9d8026007f049cd5cedb2cb81065c4d
Armaansingh4886 <armaansingh4886@gmail.com>
2024-01-30 23:08:32 +0530

    removing errors

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,16 +1,16 @@
-import React, { useContext, useEffect, useRef } from 'react'
+import React, { useContext} from 'react'
 import ListItem from './ListItem'
 import{ TasksContext }from "../App"
 const List = () => {
- const { task, setTask ,updateTask} = useContext(TasksContext);
+ const { task} = useContext(TasksContext);
  
- const todolists = task.map((item,index)=><ListItem key={index} name ={index} title={ item[0]} desc = { item[1]} time={item[2]}/>)
+ const todolists = task.map((item,index)=><ListItem key={index} name ={index} title={ item[0]} desc = { item[1]} time={ item[2]}/>)
  
   return (
     <div className='list'>
    
       {todolists}
-
+ 
     </div>
   )
 }
