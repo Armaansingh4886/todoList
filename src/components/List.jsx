9fe3f976e9125d229a6792145dba7d8f43c688fe
Armaansingh4886 <armaansingh4886@gmail.com>
2024-02-01 15:20:58 +0530

    search option

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,16 +1,16 @@
-import React, { useContext} from 'react'
+import React, { useContext,useState} from 'react'
 import ListItem from './ListItem'
 import{ TasksContext }from "../App"
 const List = () => {
  const { task} = useContext(TasksContext);
+ const [searchValue ,setSearchValue] = useState("");
  
- const todolists = task.map((item,index)=><ListItem key={index} name ={index} title={ item[0]} desc = { item[1]} time={ item[2]}/>)
- 
+        console.log(task[0][0].includes(searchValue))
+ const todolists = task.map((item,index)=>(searchValue===""||item[0].includes(searchValue))&&<ListItem key={index} name ={index} title={ item[0]} desc = { item[1]} time={ item[2]}/>)
   return (
     <div className='list'>
-   
+      <input type="text"placeholder='search...' onChange={(e)=>{setSearchValue(e.target.value)}}/>
       {todolists}
- 
     </div>
   )
 }
